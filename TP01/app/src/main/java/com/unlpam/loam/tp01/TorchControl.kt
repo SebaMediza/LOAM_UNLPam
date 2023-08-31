475931f0745e68cfdd750cc188656fbb3605dd31
SebaMediza <sebamediza@hotmail.com>
2023-08-31 14:30:44 -0300

    Torch, Rear and Front Camera, ready

--- TP01/app/src/main/java/com/unlpam/loam/tp01/TorchControl.kt
+++ TP01/app/src/main/java/com/unlpam/loam/tp01/TorchControl.kt
@@ -8,27 +8,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import android.widget.Toast
+
 
 class TorchControl : AppCompatActivity() {
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_torch_control)
 
         val goToMainMenu: Button = findViewById(R.id.goToMainMenuTorch)
         val cameraManager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val mySwitch: Switch = findViewById(R.id.toggleTorch)
+        val requestPermissionTorch: Button = findViewById(R.id.permissionTorch)
+
         mySwitch.setOnCheckedChangeListener { _, isToggled ->
             if (isToggled){
-                cameraManager.setTorchMode("0", true)
-            } else {
+                try {
+                    cameraManager.setTorchMode("0", true)
+                }catch (e: Exception){
+                    Toast.makeText(this@TorchControl, "Fallo al Activar el Flash", Toast.LENGTH_SHORT).show()
+                }
+            }else{
                 cameraManager.setTorchMode("0", false)
             }
         }
-
         goToMainMenu.setOnClickListener{
             val intento = Intent(this, MainActivity::class.java)
             startActivity(intento)
         }
+
+
     }
 }
