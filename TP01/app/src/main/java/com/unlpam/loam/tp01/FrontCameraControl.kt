f504059f989fbfbd65fd348cb8a90045300e44c4
SebaMediza <sebamediza@hotmail.com>
2023-09-01 10:42:15 -0300

    Rear camera ready

--- TP01/app/src/main/java/com/unlpam/loam/tp01/FrontCameraControl.kt
+++ TP01/app/src/main/java/com/unlpam/loam/tp01/FrontCameraControl.kt
@@ -1,31 +1,115 @@
 package com.unlpam.loam.tp01
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.ImageFormat
-import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
-import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CaptureRequest
-import android.media.ImageReader
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
 import android.os.Bundle
-import android.os.Environment
-import android.os.Handler
-import android.os.HandlerThread
-import android.view.Surface
-import android.view.TextureView
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.unlpam.loam.tp01.databinding.ActivityRearCameraControlBinding
 import java.io.File
-import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class FrontCameraControl : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    private lateinit var binding: ActivityRearCameraControlBinding
+    private var imageCapture: ImageCapture?=null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_front_camera_control)
+        binding = ActivityRearCameraControlBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        if (allPermissionGranted()){
+//            Toast.makeText(this,"We Have Permission", Toast.LENGTH_SHORT).show()
+            startCamera()
+        }else{
+            ActivityCompat.requestPermissions(this, Constants.REQUIRED_PERMITIONS, Constants.REQUEST_CODE_PERMISSIONS)
+        }
+        binding.btnTakePicRearCamera.setOnClickListener{
+            takePhoto()
+        }
+    }
+
+    private fun getOutputDirectory(): File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{mFile->
+            File(mFile, resources.getString(R.string.app_name).apply{
+                mFile.mkdirs()
+            })
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto(){
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(outputDirectory, SimpleDateFormat(Constants.FILE_NAME_FORMAT, Locale.getDefault()).format(System.currentTimeMillis()) + ".jpg")
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
+            object: ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+                    Toast.makeText(this@FrontCameraControl,"$msg $savedUri", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG,"${exception.message}", exception)
+                }
+            })
+    }
+
+    private fun startCamera(){
+        val cameraProviderFeature = ProcessCameraProvider.getInstance(this)
+        cameraProviderFeature.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFeature.get()
+            val preview = Preview.Builder().build().also {mPreview ->
+                mPreview.setSurfaceProvider(binding.viewFinderRearCamera.surfaceProvider)
+            }
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (e: Exception){
+                Log.d(Constants.TAG, "Fail to Start Camera", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermissionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(this,"Permissions not Granted by the User.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMITIONS.all{
+            ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
--- TP01/app/src/main/java/com/unlpam/loam/tp01/RearCameraControl.kt
+++ TP01/app/src/main/java/com/unlpam/loam/tp01/RearCameraControl.kt
@@ -1,27 +1,105 @@
 package com.unlpam.loam.tp01
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.unlpam.loam.tp01.databinding.ActivityRearCameraControlBinding
-import com.unlpam.loam.tp01.databinding.ActivityTorchControlBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class RearCameraControl : AppCompatActivity() {
 
     private lateinit var binding: ActivityRearCameraControlBinding
+    private var imageCapture: ImageCapture?=null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRearCameraControlBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
         if (allPermissionGranted()){
-            Toast.makeText(this,"We Have Permission", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this,"We Have Permission", Toast.LENGTH_SHORT).show()
+            startCamera()
         }else{
             ActivityCompat.requestPermissions(this, Constants.REQUIRED_PERMITIONS, Constants.REQUEST_CODE_PERMISSIONS)
+        }
+        binding.btnTakePicRearCamera.setOnClickListener{
+            takePhoto()
+        }
+    }
+
+    private fun getOutputDirectory(): File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{mFile->
+            File(mFile, resources.getString(R.string.app_name).apply{
+                mFile.mkdirs()
+            })
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto(){
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(outputDirectory, SimpleDateFormat(Constants.FILE_NAME_FORMAT, Locale.getDefault()).format(System.currentTimeMillis()) + ".jpg")
+        val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(outputOption, ContextCompat.getMainExecutor(this),
+            object: ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+                    Toast.makeText(this@RearCameraControl,"$msg $savedUri", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(Constants.TAG,"${exception.message}", exception)
+                }
+            })
+    }
+
+    private fun startCamera(){
+        val cameraProviderFeature = ProcessCameraProvider.getInstance(this)
+        cameraProviderFeature.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFeature.get()
+            val preview = Preview.Builder().build().also {mPreview ->
+                mPreview.setSurfaceProvider(binding.viewFinderRearCamera.surfaceProvider)
+            }
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }catch (e: Exception){
+                Log.d(Constants.TAG, "Fail to Start Camera", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermissionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(this,"Permissions not Granted by the User.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
     }
 
@@ -30,4 +108,8 @@ class RearCameraControl : AppCompatActivity() {
             ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
         }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 }
